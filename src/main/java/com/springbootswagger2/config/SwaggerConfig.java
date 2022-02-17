c3168aed22f18b0d95eb506ebaeb63afa71466ad
leonardevans <lemutugi@gmail.com>
2022-02-17 09:47:11 -0500

    spring boot + profiles

--- src/main/java/com/springbootswagger2/config/SwaggerConfig.java
+++ src/main/java/com/springbootswagger2/config/SwaggerConfig.java
@@ -4,6 +4,7 @@ import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -11,24 +12,27 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@Profile("swagger-enabled-for-qa")
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+
     @Bean
-    public Docket postsApi(){
-        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api").apiInfo(apiInfo()).select().paths(postPaths()).build();
+    public Docket postsApi() {
+        return new Docket(DocumentationType.SWAGGER_2).groupName("public-api")
+                .apiInfo(apiInfo()).select().paths(postPaths()).build();
     }
 
     private Predicate<String> postPaths(){
         return Predicates.or(PathSelectors.regex("/api/posts.*"), PathSelectors.regex("/api/hello.*"));
     }
 
-    private ApiInfo apiInfo(){
+    private ApiInfo apiInfo() {
         return new ApiInfoBuilder().title("Spring Boot Swagger 2")
                 .description("Using swagger2 to document Spring Boot REST APIs")
                 .termsOfServiceUrl("http://bedubiz.com")
                 .contact("bizbedu@gmaal.com").license("Bedubiz License")
                 .licenseUrl("bizbedu@gmaal.com").version("1.0").build();
     }
+
 }
