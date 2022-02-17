631fb6a3c813cc04184fcb5cb280ab0f9c93c791
leonardevans <lemutugi@gmail.com>
2022-02-17 10:11:20 -0500

    @ApiOperation

--- src/main/java/com/springbootswagger2/controller/HelloController.java
+++ src/main/java/com/springbootswagger2/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.springbootswagger2.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @RequestMapping(method = RequestMethod.GET, value = "/api/hello")
+    @ApiOperation(value = "getGreeting", notes = "get getGreetings", nickname = "getGreeting")
     public String sayHello() {
         return "Swagger Hello World";
     }
