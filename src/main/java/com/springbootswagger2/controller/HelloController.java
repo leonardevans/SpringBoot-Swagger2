38e60b09847b5b2adc3d1622fb88e9490774eb13
leonardevans <lemutugi@gmail.com>
2022-02-23 05:02:05 -0500

    @ApiParam

--- src/main/java/com/springbootswagger2/controller/HelloController.java
+++ src/main/java/com/springbootswagger2/controller/HelloController.java
@@ -2,22 +2,25 @@ package com.springbootswagger2.controller;
 
 import com.springbootswagger2.Hello;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-    @RequestMapping(method = RequestMethod.GET, value = "/api/hello")
+    @RequestMapping(method = RequestMethod.GET, value = "/api/hello/{name}")
     @ApiOperation(value = "getGreeting", notes = "get getGreetings", nickname = "getGreeting")
     @ApiResponses(value = {
             @ApiResponse(code = 500, message = "Server error"),
             @ApiResponse(code = 404, message = "Service not found"),
             @ApiResponse(code = 200, message = "Successful retrieval",
                     response = Hello.class, responseContainer = "List") })
-    public String sayHello() {
+    public String sayHello(@ApiParam(value = "testId",
+            required = true, defaultValue = "111")  @PathVariable("name ") final String name) {
         return "Swagger Hello World";
     }
 }
